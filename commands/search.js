c688251a01eeaa5765f791f0c491887193762db2
John Facey <john.facey@gmail.com>
2021-12-24 18:36:49 -0600

    patched search display results

--- commands/search.js
+++ commands/search.js
@@ -18,24 +18,27 @@ module.exports = {
         let iSave = 0
         let count = 0;
         var feedArray = libFlayer.getFeeds();
+        var searchString = "";
         feedArray.forEach(linkFlay => {
             if (linkFlay.title.toLowerCase().indexOf(search.toLowerCase()) > -1) {
                 iSave = i;
                 found = true;
                 console.log(linkFlay.title);
-                message.reply(`Use !get ${i} to view: ${linkFlay.title}`);
+                searchString += `Use !get ${i} to view: ${linkFlay.title} \n`;
                 count++;
             }
             i++;
             
         });
+
+        message.reply(searchString);
         if (count == 1) {
             //message.channel.send('Displaying 1 result');
             //message.channel.send('!get '+iSave);
         }
 
         if (count > 0) {
-            message.channel.send(`Displaying results for: ${search}`);
+            message.channel.send(`Displayed results for: ${search}`);
         }
         
         if (!found) {
